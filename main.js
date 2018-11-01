e028bd4ab7e64341b1bbe07c65310736427da346
MalteSaup <Malte.Saupe>
2018-11-01 19:09:52 +0100

    Protoytpe Website

---
+++ main.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    var buttonArray = document.getElementsByClassName("buttonField");
+    for(var i = 0; i < buttonArray.length; i++){
+        buttonArray[i].id;
+        buttonArray[i].addEventListener('click', function(e){
+            if(startPressed){
+                console.log(e.target.id);
+                document.getElementById(e.target.id).style.backgroundColor = "black";    
+            }
+        });
+
+    }
+    
+    var startButton = document.getElementById("startButton");
+    var startPressed = false;
+    startButton.addEventListener('click', function(){
+        for(var i = 0; i < buttonArray.length; i++){
+            buttonArray[i].style.backgroundColor = "white";
+        }
+        if(!startPressed){
+            startButton.innerHTML = "Reset";
+            startPressed = true;
+        }
+    })
+}
