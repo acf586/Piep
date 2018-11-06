e3fffef231d124a1bfb0e844d678444c8237dfca
Christian Schröder <christian.schroeder@haw-hamburg.de>
2018-11-06 09:42:41 +0100

    daynamisches Spielfeld

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
-var buttonArray = document.getElementsByClassName("fieldButton");
+var buttonArray = null;
+var startPressed = false;
+var startButton;
 
 window.onload = function(){
-    
-    
-    for(var i = 0; i < buttonArray.length; i++){
-        buttonArray[i].id;
-        buttonArray[i].addEventListener('click', function(e){
-            if(startPressed){
-                console.log(e.target.id);
-                document.getElementById(e.target.id).style.backgroundColor = "black";   
-            }
-        });
 
-    }
+startButton = document.getElementById("startButton");
     
-    var startButton = document.getElementById("startButton");
-    var startPressed = false;
+document.body.style.backgroundImage = "url('Assets/Background.png')";
+    
+createGameField();
 
-    startButton.addEventListener('click', function(){
-        resetGameField();
+addEventListener();
 
-        if(!startPressed){
-            startButton.innerHTML = "Reset";
-            startPressed = true;
-        }
-    });
-    document.body.style.backgroundImage = "url('Assets/Background.png')";
+initializeStartButton();
    
 }
 
@@ -37,7 +24,6 @@ function resetGameField(){
         buttonArray[i].style.borderColor = "black";
         buttonArray[i].style.border = "solid";
         
-
     }
     let xIndex = (Math.random()*4).toFixed(0);
     let yIndex = (Math.random()*4).toFixed(0);
@@ -46,4 +32,43 @@ function resetGameField(){
 
     document.getElementById(xIndex + yIndex).style.backgroundImage = "url('Assets/Player.png')";
     document.getElementById(xIndex + yIndex).style.border = "none";
+}
+
+function createGameField(){
+
+    for(var i = 0; i < 25; i++){
+        fieldButton = document.createElement("div");
+        fieldButton.className = "fieldButton";
+        var fieldId = parseInt(i/5) + "" + i%5;
+        fieldButton.setAttribute("id", fieldId);
+
+        document.getElementById("game-grid").appendChild(fieldButton);
+        
+    }
+    buttonArray = document.getElementsByClassName("fieldButton");
+}
+
+function addEventListener(){
+    for(var i = 0; i < buttonArray.length; i++){
+        buttonArray[i].id;
+        buttonArray[i].addEventListener('click', function(e){
+            if(startPressed){
+                document.getElementById(e.target.id).style.backgroundColor = "black";   
+                console.log(e.target.id);
+            }
+               
+        });
+
+    }
+}
+
+function initializeStartButton(){
+    startButton.addEventListener('click', function(){
+        resetGameField();
+
+        if(!startPressed){
+            startButton.innerHTML = "Reset";
+            startPressed = true;
+        }
+    });
 }
