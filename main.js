f8223ed7012d5e598194c5285324b9c37123de3c
malte.saupe@haw-hamburg.de <malte.saupe@haw-hamburg.de>
2018-11-21 12:59:43 +0100

    GO Screen in StartScreen geändert und angefangen GO Screen zu erstellen.

--- main.js
+++ main.js
@@ -15,8 +15,9 @@ var gameContainer,
     control,
     wrapper;
 
+var startScreen = false;
 var gameOverScreen = false;
-var startet = false;
+
 window.onload = function () {
     initializeWebsite();
 
@@ -27,7 +28,7 @@ window.onload = function () {
     fieldButtonsAddEventListener();
 
     createTestButtons();
-
+    
 }
 
 function initializeWebsite(){
@@ -36,18 +37,19 @@ function initializeWebsite(){
 
     createGameField();
 
+    createStartScreen();
+
     updateSize();
 
     initializePositionArray();
 }
 
-function createGameOverScreen(){
+function createStartScreen(){
     
-    var wrapper = document.getElementById("wrapper").style;
-    var marginOld = wrapper.marginTop;
-    console.log(marginOld);
+    var marginOld = parseFloat(document.getElementById("wrapper").style.marginTop);
+    marginOld *= (-1);
     
-    gameOverScreen = true;
+    startScreen = true;
     
     var overlay = document.createElement("div");
     var button = document.createElement("div");
@@ -59,27 +61,67 @@ function createGameOverScreen(){
     document.body.appendChild(overlay);
     
     overlay = document.getElementById("overlay");
-    gOButton = document.getElementById("overlay_button");
+    startButton = document.getElementById("overlay_button");
     
-    overlay.style.marginTop = -marginOld;
+    overlay.style.marginTop = marginOld;
     
-    if(startet){
-        gOButton.innerHTML = "Restart";
-    }
-    else{
-        gOButton.innerHTML = "Start";
-    }
-    gOButton.addEventListener('click', function (e){
-        wrapper.display = "block";
-        gOButton.style.animationName = "buttonPuls, fadeOut";
+    startButton.innerHTML = "Start";
+    
+    startButton.addEventListener('click', function (e){
+        startButton.style.animationName = "buttonPuls, fadeOut";
         overlay.style.animationName = "fadeOut";
-        
+        startScreen = false;
+        newGame.startPressed();
         setTimeout(function () {
-            wrapper.marginTop = marginOld;
             overlay.remove();
             }, 2000)
     });
     
+}
+
+function createGameOverScreen(){
+    
+    var marginOld = parseFloat(document.getElementById("wrapper").style.marginTop);
+    marginOld *= (-1);
+    
+    gameOverScreen = true;
+    
+    var overlay = document.createElement("div");
+    var button = document.createElement("div");
+    var pointTextField = document.createElement("div");
+    var overlayWrap = document.createElement("div");
+    
+    overlay.setAttribute("id", "overlay")
+    button.setAttribute("id", "overlay_button");
+    pointTextField.setAttribute("id", "overlay_textfield");
+    overlayWrap.setAttribute("id", "overlay_wrapper");
+    
+    overlayWrap.appendChild(pointTextField);
+    overlayWrap.appendChild(button);
+    overlay.appendChild(overlayWrap);
+    document.body.appendChild(overlay);
+    
+    overlay = document.getElementById("overlay");
+    overlayWrap = document.getElementById("overlayWrap");
+    gameOverButton = document.getElementById("overlay_button");
+    pointTextField = document.getElementById("overlay_textfield");
+    
+    
+    overlay.style.marginTop = marginOld;
+    
+    gameOverButton.innerHTML = "Restart";
+    pointTextField.innerHTML = "Punkte: ";
+    
+    
+    
+    gameOverButton.addEventListener('click', function (e){
+        gameOverButton.style.animationName = "buttonPuls, fadeOut";
+        overlay.style.animationName = "fadeOut";
+        newGame.startPressed();
+        setTimeout(function () {
+            overlay.remove();
+            }, 2000)
+    });
     
     updateSize();
     
@@ -115,17 +157,37 @@ function updateSize() {
 
         menu.style.height = winWidth / 2;
         control.style.height = winWidth / 2;
-
         margin = (winHeight - winWidth / 2) / 2;
         wrapper.style.marginTop = margin;
-        if(gameOverScreen){
-            var gOButton = document.getElementById("overlay_button");
+        if(startScreen || gameOverScreen){
+            var button = document.getElementById("overlay_button");
             
-            gOButton.style.height = winWidth * 0.2; 
-            gOButton.style.width = winWidth * 0.2;
-            gOButton.style.marginTop = (winHeight-winWidth * 0.2) / 2;
+            if(startScreen){
+                
+                button.style.height = winWidth * 0.2; 
+                button.style.width = winWidth * 0.2;
+                button.style.marginTop = (winHeight-winWidth * 0.2) / 2;
+                button.style.lineHeight = (winWidth * 0.2) + "px";
             
-            gOButton.style.lineHeight = (winWidth * 0.2) + "px";
+            }
+            
+            else{
+                
+                var overlayWrap = document.getElementById("overlay_wrapper");
+                var textField = document.getElementById("overlay_textfield");
+                console.log(overlayWrap.clientHeight);
+                
+                button.style.height = winWidth * 0.15; 
+                button.style.width = winWidth * 0.15;
+                button.style.lineHeight = (winWidth * 0.15) + "px";
+            
+                textField.style.width = winWidth * 0.2;
+                textField.style.height = winWidth * 0.1;
+                textField.style.lineHeight = winWidth * 0.1 + "px";
+                
+                overlayWrap.style.marginTop = (winHeight-overlayWrap.clientHeight) / 2;
+                
+            }
             
             document.getElementById("overlay").style.marginTop = margin * -1;
         }
@@ -141,24 +203,47 @@ function updateSize() {
         margin = 0;
         wrapper.style.marginTop = margin;
         
-        if(gameOverScreen){
-            var gOButton = document.getElementById("overlay_button");
+        if(startScreen || gameOverScreen){
+            var button = document.getElementById("overlay_button");
             
-            gOButton.style.height = winHeight * 0.4; 
-            gOButton.style.width = winHeight * 0.4;
-            gOButton.style.marginTop = (winHeight - winHeight * 0.4) / 2;
+            if(startScreen){
+                
+                button.style.height = winHeight * 0.4; 
+                button.style.width = winHeight * 0.4;
+                button.style.marginTop = (winHeight - winHeight * 0.4) / 2;
+                button.style.lineHeight = (winHeight * 0.4) + "px";
             
-            gOButton.style.lineHeight = (winHeight * 0.4) + "px";
+            }
+            
+            else{
+                
+                var textField = document.getElementById("overlay_textfield");
+                var overlayWrap = document.getElementById("overlay_wrapper");
+                
+                button.style.height = winHeight * 0.3; 
+                button.style.width = winHeight * 0.3;
+                button.style.lineHeight = (winHeight * 0.3) + "px";
+            
+                textField.style.width = winHeight * 0.5;
+                textField.style.height = winHeight * 0.2;
+                textField.style.lineHeight = winHeight * 0.2 + "px";
+                
+                overlayWrap.style.marginTop = (winHeight - overlayWrap.clientHeight) / 2;
+                
+                console.log(overlayWrap.clientHeight);
+                
+            }
+            
             
             document.getElementById("overlay").style.marginTop = margin * -1;
         }
     }
     
     /*if(gameOverScreen){
-        var gOButton = document.getElementById(overlay_button);
-        gOButton.style.height = 
-        gOButton.style.width = 
-        gOButton.style.marginTop =     
+        var startButton = document.getElementById(overlay_button);
+        startButton.style.height = 
+        startButton.style.width = 
+        startButton.style.marginTop =     
     }*/
     
     
